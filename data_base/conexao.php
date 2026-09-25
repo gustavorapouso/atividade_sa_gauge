4b53d9eda4e1953e698f25537c56b3fc3d83afc2
gustavorapouso <gustavo_rapouso@estudante.sesisenai.org.br>
2026-09-25 08:27:59 -0300

    root delete

--- data_base/conexao.php
+++ data_base/conexao.php
@@ -1,7 +1,7 @@
 <?php
     $servidor = 'localhost';
     $usuario = 'root';
-    $senha = 'root';
+    $senha = '';
     $banco = 'ferrorama_gauge';
 
     $conexao = new mysqli($servidor, $usuario, $senha, $banco);
